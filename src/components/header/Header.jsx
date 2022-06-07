5a626a05dbaf83c775281eb94858fadf9d3a27c4
sahuyash1103 <sahuyash1103@gmail.com>
2022-06-07 19:59:26 +0530

    fixed:nav bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RiShoppingCartLine } from "react-icons/ri";
-import { BiSearchAlt2 } from "react-icons/bi";
+import { AiOutlineSearch } from "react-icons/ai";
+import { IoMdArrowDropdown } from "react-icons/io";
 import "./Header.css";
 
 function Header() {
@@ -12,13 +13,19 @@ function Header() {
         <span className="header_deliveryAddr">choose location</span>
       </div>
       <div className="header_search">
-          <div className="header_searchCatogory">All^</div>
+          <div className="header_searchCatogory">
+            <span>All</span>
+          <span><IoMdArrowDropdown/></span>
+          </div>
         <input className="header_searchInput" type="text" />
-        <BiSearchAlt2 className="header_searchIcon" />
+        <AiOutlineSearch className="header_searchIcon" />
       </div>
       <div className="header_nav">
-          {/* TODO */}
-        <div className="header_option">lang^</div>
+        <div className="header_optionLang">
+          <span>Eng</span>
+          <IoMdArrowDropdown className="header_optionLangArrow"/>
+
+        </div>
         <div className="header_option">
           <span className="header_optionLineOne">Hello, Guest</span>
           <span className="header_optionLineTwo">Sign In</span>
@@ -27,14 +34,14 @@ function Header() {
           <span className="header_optionLineOne">Returns</span>
           <span className="header_optionLineTwo">& Order</span>
         </div>
-        <div className="header_option">
+        {/* <div className="header_option">
           <span className="header_optionLineOne">Your</span>
           <span className="header_optionLineTwo">Prime</span>
-        </div>
+        </div> */}
 
         <div className="header_optionBasket">
-          <span className="header_optionLineTwo header_basketCount">0</span>
           <RiShoppingCartLine className="header_cart" />
+          <span className="header_basketCount">0</span>
         </div>
       </div>
     </div>
