ace33abe33b678389b473f912dc26c3804d5415a
sahuyash1103 <sahuyash1103@gmail.com>
2022-06-08 22:19:21 +0530

    added:home page and checkout page with routing

---
+++ src/components/home/product/Product.jsx
@@ -0,0 +1,36 @@
+
+import React from "react";
+import "./Product.css";
+
+function Product({ id, title, image, price, rating }) {
+
+  // console.log(basket);
+  const addToBasket = () => {
+  
+  };
+
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p key={i}>🌟</p>
+            ))}
+        </div>
+      </div>
+
+      <img src={image} alt="" />
+
+      <button onClick={addToBasket}>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
